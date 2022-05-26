b30f8824e79d01273e3b6a0c56a1439c7242f5fe
Doze <69562683+kuroroAsync@users.noreply.github.com>
2022-05-25 23:54:14 -0400

    25/05/2022

--- Seccion_3/multipleExtraccion.py
+++ Seccion_3/multipleExtraccion.py
@@ -1,12 +1,28 @@
 from bs4 import BeautifulSoup
 import requests as rq
 
-web = 'https://subslikescript.com/movie/Titanic-120338'
+root = 'https://subslikescript.com' # ROOT (Raiz)
+web = f'{root}/movies'
 get = rq.get(web)
 content = get.text
 soup = BeautifulSoup(content, 'lxml')
-box = soup.find('article', class_= 'main-article')
-h1 = box.find('h1').get_text()
-script = box.find('div',class_='full-script').get_text(strip=True, separator=' ')
-with open(f'Seccion_3/{h1}.txt','w',encoding='utf-8') as f:
-    f.write(script)
+box = soup.find('article', class_='main-article') 
+links = [] 
+for link in box.find_all('a', href=True): # Conseguir todos los elementos de un elemento.
+    links.append(link['href'])
+for link in links:
+    web = f'{root}/{link}'
+    get = rq.get(web)
+    content = get.text
+    soup = BeautifulSoup(content, 'lxml')
+    box = soup.find('article', class_='main-article') 
+    h1 = box.find('h1').get_text()
+    script = box.find('div',class_= 'full-script').get_text(strip=True, separator=' ')
+    try:
+        with open(f'Seccion_3/scripts/{str(h1)}.txt','w') as arch:
+            arch.write(str(script))
+    except UnicodeEncodeError:
+        with open(f'Seccion_3/scripts/{str(h1)}.txt','w', encoding='utf-8') as arch:
+            arch.write(str(script))
+print("Proceso terminado")
+        
